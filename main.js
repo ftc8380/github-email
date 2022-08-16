965e05a8039579e5b1e076b992c738780910b39a
forkpoweroutlet <48189055+forkpoweroutlet@users.noreply.github.com>
2022-08-16 10:31:29 -0700

    Clipboard and other changes

--- main.js
+++ main.js
@@ -1,28 +1,48 @@
-var textField = document.getElementById("username");
+var textField = document.getElementsByTagName("input")[0];
+var emailDisp = document.getElementsByTagName("span")[0];
 
 textField.addEventListener("keyup", ({key}) => {
-    if (key === "Enter") {
-        loadXMLDoc();
+    if (key !== "Enter") {
+        return;
     }
-});
 
-function loadXMLDoc() {
     var username = textField.value;
 
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
+    xhttp.open("GET", "https://api.github.com/users/" + username, true);
+    xhttp.send();
 
-        var resp = JSON.parse(this.responseText);
-        var id = resp.id.toString();
-        var email = resp.id.toString() + "+" + username + "@users.noreply.github.com";
-        document.getElementById("email").innerHTML = email;
+    xhttp.onloadend = function() {
+        var button = document.getElementsByTagName("button")[0];
 
-    } else if(this.status == 404) {
-        document.getElementById("email").innerHTML = "Invalid GitHub username";
-    }
-  };
-  
-  xhttp.open("GET", "https://api.github.com/users/" + username, true);
-  xhttp.send();
-}
+        if (this.readyState == 4 && this.status === 200) {
+
+            var resp = JSON.parse(this.responseText);
+            var email = resp.id.toString() + "+" + resp.login + "@users.noreply.github.com";
+
+            emailDisp.innerHTML = email;
+            button.style.display = "inline";
+
+        } else if(this.status == 404) {
+            emailDisp.innerHTML = "Invalid GitHub username";
+            button.style.display = "none";
+        }
+
+        document.getElementById('post').style.display = 'block';
+        document.getElementById('notice').style.display = 'block';
+
+    };
+});
+
+var button = document.getElementsByTagName("button")[0];
+button.addEventListener("click", () => {
+    navigator.clipboard.writeText(emailDisp.innerHTML);
+
+    var width = button.offsetWidth;
+    button.innerHTML = "✔";
+    button.style.width = width.toString() + "px";
+    
+    setTimeout(function() {
+        button.innerHTML = "Copy";
+    }, 1000);
+});
